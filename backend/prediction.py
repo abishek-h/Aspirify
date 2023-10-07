428220117bbab78a9493583a112f1425a2cb5edb
cr4yth <abixshek@gmail.com>
2023-10-07 16:29:07 +0530

    init

---
+++ backend/prediction.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request, jsonify
+import json
+import pickle
+import numpy as np
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+
+global datum
+global y
+@app.route('/prediction',methods = ['POST'])
+@cross_origin()
+def result():
+    datum = request.get_json()
+    y = datum["arr"]
+    arr = [eval(z) for z in y]
+    data = np.array(arr)
+    data = data.reshape(1,-1)
+    print(data)
+    loaded_model = pickle.load(open("careerlast.pkl", 'rb'))
+    predictions = loaded_model.predict(data)  
+    print(predictions)
+    reso = predictions[0]
+    print(reso)
+    pred = loaded_model.predict_proba(data)
+    print(pred)
+      #acc=accuracy_score(pred,)
+    pred = pred > 0.05
+      #print(predictions)
+    i = 0
+    j = 0
+    index = 0
+    res = {}
+    final_res = {}
+    while j < 17:
+        if pred[i, j]:
+            res[index] = j
+            index += 1
+        j += 1
+    print(j)
+    print(res)
+    index = 0
+    for key, values in res.items():
+        if values != predictions[0]:
+            final_res[index] = values
+            print('final_res[index]:',final_res[index])
+            index += 1
+        print(final_res)
+    jobs_dict = {0:'AI ML Specialist',
+                   1:'API Integration Specialist',
+                   2:'Application Support Engineer',
+                   3:'Business Analyst',
+                   4:'Customer Service Executive',
+                   5:'Cyber Security Specialist',
+                   6:'Data Scientist',
+                   7:'Database Administrator',
+                   8:'Graphics Designer',
+                   9:'Hardware Engineer',
+                   10:'Helpdesk Engineer',
+                   11:'Information Security Specialist',
+                   12:'Networking Engineer',
+                   13:'Project Manager',
+                   14:'Software Developer',
+                   15:'Software Tester',
+                   16:'Technical Writer'}
+                
+    job = {}
+      #job[0] = jobs_dict[predictions[0]]
+    index = 1
+
+    print(final_res)
+    finalarray = []
+    for key in final_res:
+        finalarray.append(jobs_dict[final_res[key]])
+    print(finalarray)
+    return jsonify({
+        "role": finalarray
+    })
+      
+if __name__ == '__main__':
+   app.run(debug = True)

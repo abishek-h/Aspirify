1b826f9655f3807e5ff42094c238073ea31a412d
cr4yth <abixshek@gmail.com>
2023-10-08 13:08:53 +0530

    mcc

--- server/venv/Scripts/activate.bat
+++ server/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\abixshek\Desktop\minorproject-nextjs\backend\venv
+set VIRTUAL_ENV=C:\Users\abixshek\Desktop\minorproject-nextjs\server\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
